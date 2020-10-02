57a60f9c625564966c86c2dadf6b044e92f5d283
surajsrv11 <surajvishwakarma625@gmail.com>
2020-10-02 16:23:55 +0530

    Updates

---
+++ precache-manifest.c6f7b14b27fec16ab943d2b01eb9ba16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ad46e8f41fa6a6c2f0f0fe4f9ee2ac",
+    "url": "/covid19-info/index.html"
+  },
+  {
+    "revision": "22872e180368281c14c7",
+    "url": "/covid19-info/static/css/main.9aa73b09.chunk.css"
+  },
+  {
+    "revision": "de556854f4ba873b5aa9",
+    "url": "/covid19-info/static/js/2.9fb81d2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19-info/static/js/2.9fb81d2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22872e180368281c14c7",
+    "url": "/covid19-info/static/js/main.1dd629c5.chunk.js"
+  },
+  {
+    "revision": "34be978e4d6fc37276b2",
+    "url": "/covid19-info/static/js/runtime-main.3e15b102.js"
+  }
+]);
